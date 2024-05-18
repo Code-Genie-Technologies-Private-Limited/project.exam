b3b51dfa9047050781a18cf6eb0fcdd03deae532
Kuldeep Kumar <kumar.kuldeep.ec.067@gmail.com>
2024-05-18 16:22:17 +0530

    Subject Crud by KD

---
+++ resources/views/dashboard/subjects/edit.blade.php
@@ -0,0 +1,43 @@
+@extends('dashboard.base')
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Edit Subject</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('subjects.update', $subject->id) }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group row">
+                                <label>Name</label>
+                                <input class="form-control" type="text" placeholder="Name" name="name" required autofocus value="{{ $subject->name }}" />
+                            </div>
+                            <div class="form-group row">
+                                <label>Status</label>
+                                <input class="form-control" type="text" placeholder="Status" name="status" required value="{{ $subject->status }}" />
+                            </div>
+                            <div class="form-group row">
+                                <label>Order</label>
+                                <input class="form-control" type="text" placeholder="Order" name="order" required value="{{ $subject->order }}" />
+                            </div>
+                            <button class="btn btn-success" type="submit">Edit</button>
+                            <a href="{{ route('subjects.index') }}" class="btn btn-primary">Return</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('javascript')
+
+@endsection
