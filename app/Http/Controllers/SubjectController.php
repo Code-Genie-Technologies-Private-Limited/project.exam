dd3948c210e3da086b6fdf014cbef917137f5093
Your Name <you@example.com>
2024-05-18 17:38:30 +0530

    subject crud byshivam

---
+++ app/Http/Controllers/SubjectController.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSubjectRequest;
+use App\Http\Requests\UpdateSubjectRequest;
+use App\Models\Subject;
+use Illuminate\Support\Facades\Validator;
+
+class SubjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:160',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $subjects = Subject::all();
+        return view('dashboard.subjects.index', compact('subjects'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSubjectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSubjectRequest $request)
+    {
+        Subject::create($request->all());
+        return redirect()->route('subjects.index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subject $subject)
+    {
+        return view('dashboard.subjects.show', compact('subject'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subject $subject)
+    {
+        return view('dashboard.subjects.edit', compact('subject'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSubjectRequest  $request
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSubjectRequest $request, Subject $subject)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3|max:160',
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        }
+        $subject->update($request->all());
+        return redirect()->route('subjects.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Subject  $subject
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subject $subject)
+    {
+        $subject->delete();
+        return redirect()->route('subjects.index');
+    }
+}
