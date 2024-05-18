54ba258aee2f3003d7ae6f019fc937335e602fdf
Your Name <you@example.com>
2024-05-18 17:41:59 +0530

    edit index blade crud by shivam

--- resources/views/dashboard/subject/edit.blade.php
+++ resources/views/dashboard/subject/edit.blade.php
@@ -7,29 +7,29 @@
                 <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
                     <div class="card">
                         <div class="card-header">
-                            <h4>Edit Student</h4>
+                            <h4>Edit Subject</h4>
                         </div>
                         <div class="card-body">
-                            <form method="POST" action="{{ route('students.update', $student->id) }}">
+                            <form method="POST" action="{{ route('subjects.update', $subject->id) }}">
                                 @csrf
                                 @method('PUT')
                                 <div class="form-group row">
                                     <label>Name</label>
                                     <input class="form-control" type="text" placeholder="Name" name="name" required
-                                        autofocus value="{{ $student->name }}" />
+                                        autofocus value="{{ $subject->name }}" />
                                 </div>
                                 <div class="form-group row">
-                                    <label>Mobile Number</label>
-                                    <input class="form-control" type="text" placeholder="Mobile Number"
-                                        name="mobile_number" required value="{{ $student->mobile_number }}" />
+                                    <label>Status</label>
+                                    <input class="form-control" type="text" placeholder=" Status" name="mobile_number"
+                                        required value="{{ $subject->mobile_number }}" />
                                 </div>
                                 <div class="form-group row">
-                                    <label>Roll Number</label>
-                                    <input class="form-control" type="text" placeholder="Roll Number" name="roll_number"
-                                        required value="{{ $student->roll_number }}" />
+                                    <label>Created By</label>
+                                    <input class="form-control" type="text" placeholder="Created By" name="created_by"
+                                        required value="{{ $subject->roll_number }}" />
                                 </div>
                                 <button class="btn btn-success" type="submit">Edit</button>
-                                <a href="{{ route('students.index') }}" class="btn btn-primary">Return</a>
+                                <a href="{{ route('subjects.index') }}" class="btn btn-primary">Return</a>
                             </form>
                         </div>
                     </div>
