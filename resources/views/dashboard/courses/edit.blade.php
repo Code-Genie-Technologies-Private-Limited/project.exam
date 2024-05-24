3e5c726e3c2db2aecae7a38eafdd178c046e46b6
Kuldeep Kumar <kumar.kuldeep.ec.067@gmail.com>
2024-05-24 10:33:46 +0530

    Course Crud completed by KD

---
+++ resources/views/dashboard/courses/edit.blade.php
@@ -0,0 +1,52 @@
+@extends('dashboard.base')
+
+@section('content')
+
+<div class="container-fluid">
+    <div class="animated fadeIn">
+        <div class="row">
+            <div class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                <div class="card">
+                    <div class="card-header">
+                        <h4>Edit Course</h4>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('courses.update', $course->id) }}">
+                            @csrf
+                            @method('PUT')
+                            <div class="form-group row">
+                                <label>Name</label>
+                                <input class="form-control" type="text" placeholder="Name" name="name" required autofocus value="{{ $course->name }}" />
+                            </div>
+                            <div class="form-group row">
+                                <label class="col-md-3 col-form-label">Status</label>
+                                <div class="col-md-9 col-form-label">
+                                    <div class="form-check">
+                                        <input class="form-check-input" id="active" type="radio" value="1" name="status" {{$course->status == 0 ? 'checked':''}}>
+                                        <label class="form-check-label" for="active">Active</label>
+                                    </div>
+                                    <div class="form-check">
+                                        <input class="form-check-input" id="inactive" type="radio" value="0" name="status" {{$course->status == 0 ? 'checked':'' }}>
+                                        <label class="form-check-label" for="inactive">Inactive</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-group row">
+                                <label>Order</label>
+                                <input class="form-control" type="text" placeholder="order" name="order" required value="{{ $course->order }}" />
+                            </div>
+                            <button class="btn btn-success" type="submit">Edit</button>
+                            <a href="{{ route('courses.index') }}" class="btn btn-primary">Return</a>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
+
+@section('javascript')
+
+@endsection
