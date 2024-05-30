91604da7573fd35831c2c9b43de190d20a21493f
Kuldeep Kumar <kumar.kuldeep.ec.067@gmail.com>
2024-05-30 19:41:35 +0530

    question crud refactored

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+use App\Models\Subject;
+use App\Models\Topic;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class QuestionFactory extends Factory
@@ -14,7 +17,15 @@ class QuestionFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'subject_id' => Subject::factory()->create()->id,
+            'topic_id' => Topic::factory()->create()->name,
+            // 'difficulty_level' => $this->faker,
+            'keyword' => $this->faker->sentence,
+            'options' => $this->faker->sentence,
+            'answer' => $this->faker->sentence,
+            'order' => $this->faker->randomfloat(2, 1, 100),
+            'status' => $this->faker->boolean,
+            'created_by' => User::factory(),
         ];
     }
 }
