25ff0deebf0ae2c4fd87717149a4a7cc1e65206d
Your Name <you@example.com>
2024-06-01 22:26:49 +0530

    subcourse

--- app/Http/Requests/UpdateSubCourseRequest.php
+++ app/Http/Requests/UpdateSubCourseRequest.php
@@ -25,7 +25,7 @@ class UpdateSubCourseRequest extends FormRequest
     {
         return [
             'name' => 'required|min:3|max:160',
-            'course_id' => 'required|integer|exists:subjects,id',
+            'course_id' => 'required|integer|exists:courses,id',
             'status' => 'required|boolean',
             'order' => 'required|numeric'
         ];
