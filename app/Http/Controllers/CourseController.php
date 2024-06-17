47d75e7ad55426d69aee2ff460fc770d07c75a31
Your Name <you@example.com>
2024-06-18 00:25:42 +0530

    course filter

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreCourseRequest;
+use App\Http\Requests\UpdateCourseRequest;
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $courses = Course::with('creator')
+            ->orderBy('order')
+            ->paginate(10);
+
+        return view('dashboard.courses.index', compact('courses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('dashboard.courses.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCourseRequest $request)
+    {
+        Course::create(array_merge(
+            $request->validated(),
+            ['created_by' => auth()->user()->id]
+        ));
+        $request->session()->flash('message', 'Created Successfully.');
+        return redirect('courses.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Course $course)
+    {
+        return view('dashboard.courses.show', compact('course'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Course $course)
+    {
+        return view('dashboard.courses.show', compact('course'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCourseRequest $request, Course $course)
+    {
+        $course->update($request->validated());
+        $request->session()->flash('message', 'Updated Successfully.');
+        return redirect('courses.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Course $course, Request $request)
+    {
+        $course->delete();
+        $request->session()->flash('message', 'Deleted Successfully.');
+        return redirect('courses.index');
+    }
+}
