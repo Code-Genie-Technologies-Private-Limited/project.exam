4742aae1bbe2d55ed4c19b4dd93aa96b2e37d4e8
Kuldeep Kumar <kumar.kuldeep.ec.067@gmail.com>
2024-06-25 20:04:49 +0530

    Course crud complete

---
+++ app/Models/Course.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use App\ModelFilters\CourseFilter;
+use EloquentFilter\Filterable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory, Filterable;
+
+    protected $guarded = [];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($course) {
+            $maxOrder = Course::max('order');
+            $course->order = $maxOrder ? $maxOrder + 1 : 1;
+        });
+    }
+
+    public function modelFilter()
+    {
+        return $this->provideFilter(CourseFilter::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+}
---
+++ database/factories/CourseFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Course>
+ */
+class CourseFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'order' => $this->faker->randomFloat(2, 1, 100),
+            'status' => $this->faker->boolean,
+        ];
+    }
+}
