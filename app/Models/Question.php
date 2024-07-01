0c6c5b8a30673108f7cbc79402ba3bb94100e0f7
Kuldeep Kumar <kumar.kuldeep.ec.067@gmail.com>
2024-07-01 22:13:51 +0530

    changes

--- app/Models/Question.php
+++ app/Models/Question.php
@@ -2,10 +2,34 @@
 
 namespace App\Models;
 
+use App\ModelFilters\QuestionFilter;
+use EloquentFilter\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable;
+
+    protected $guarded = [];
+
+    public function modelFilter()
+    {
+        return $this->provideFilter(QuestionFilter::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function subject()
+    {
+        return $this->belongsTo(Subject::class);
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class);
+    }
 }
